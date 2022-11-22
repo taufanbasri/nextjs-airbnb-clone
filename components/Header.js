fc0382bc580bee48b7db1a5c1cd0b2563b31899a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-22 23:30:12 +0700

    Header: Left side

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import Image from "next/legacy/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* Left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* Middle */}
+      <div></div>
+
+      {/* Right */}
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
