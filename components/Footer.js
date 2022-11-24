6be7984b331f91d59437dab00e4e2196d64d2757
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-24 08:07:14 +0700

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 px-32 text-gray-600 bg-gray-100 gap-y-10 md:grid-cols-4 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a preety awesome clone</p>
+        <p>Preferrals accepted</p>
+        <p>Papafam</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a preety awesome clone</p>
+        <p>Preferrals accepted</p>
+        <p>Papafam</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a preety awesome clone</p>
+        <p>Preferrals accepted</p>
+        <p>Papafam</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
