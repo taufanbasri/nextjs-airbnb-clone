2d116af8043ae3c0f5c6f874afc0483689361bf3
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-24 12:33:23 +0700

    InfoCard Component

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,3 +1,6 @@
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
+import Image from "next/legacy/image";
 import React from "react";
 
 const InfoCard = ({
@@ -9,7 +12,42 @@ const InfoCard = ({
   price,
   total,
 }) => {
-  return <div>{location}</div>;
+  return (
+    <div className="flex px-2 pr-4 transition duration-200 ease-out border-b cursor-pointer py-7 hover:opacity-80 hover:shadow-lg first:border-t">
+      <div className="relative flex-shrink-0 w-40 h-24 md:h-52 md:w-80">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="cursor-pointer h-7" />
+        </div>
+
+        <h4 className="text-xl">{title}</h4>
+
+        <div className="w-10 pt-2 border-b"></div>
+
+        <p className="flex-grow pt-2 text-sm text-gray-500">{description}</p>
+
+        <div className="flex items-end justify-between pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" /> {star}
+          </p>
+
+          <div>
+            <p className="pb-2 text-lg font-semibold">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoCard;
